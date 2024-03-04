a65b377a2ec09be87ea89c55942fced3c8b7fca7
Oleh Zanevych <ozanevych@ancestry.com>
2024-03-04 09:57:48 +0200

    Added support of PUT, PATCH and DELETE methods for Faculty

--- src/main/java/org/lnu/teaching/web/application/dising/deanery/controller/facalty/FacultyController.java
+++ src/main/java/org/lnu/teaching/web/application/dising/deanery/controller/facalty/FacultyController.java
@@ -3,11 +3,15 @@ package org.lnu.teaching.web.application.dising.deanery.controller.facalty;
 import lombok.AllArgsConstructor;
 import org.lnu.teaching.web.application.dising.deanery.dto.faculty.BaseFacultyDto;
 import org.lnu.teaching.web.application.dising.deanery.dto.faculty.FacultyDto;
+import org.lnu.teaching.web.application.dising.deanery.dto.faculty.FacultyPatch;
 import org.lnu.teaching.web.application.dising.deanery.service.faculty.FacultyService;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -37,5 +41,23 @@ public class FacultyController {
     @GetMapping("{id}")
     public FacultyDto find(@PathVariable Long id) {
         return facultyService.find(id);
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@PathVariable Long id, @RequestBody BaseFacultyDto facultyDto) {
+        facultyService.update(id, facultyDto);
+    }
+
+    @PatchMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@PathVariable Long id, @RequestBody FacultyPatch facultyPatch) {
+        facultyService.patch(id, facultyPatch);
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        facultyService.delete(id);
     }
 }
--- src/main/java/org/lnu/teaching/web/application/dising/deanery/service/faculty/FacultyService.java
+++ src/main/java/org/lnu/teaching/web/application/dising/deanery/service/faculty/FacultyService.java
@@ -2,6 +2,7 @@ package org.lnu.teaching.web.application.dising.deanery.service.faculty;
 
 import org.lnu.teaching.web.application.dising.deanery.dto.faculty.BaseFacultyDto;
 import org.lnu.teaching.web.application.dising.deanery.dto.faculty.FacultyDto;
+import org.lnu.teaching.web.application.dising.deanery.dto.faculty.FacultyPatch;
 
 import java.util.List;
 
@@ -9,4 +10,7 @@ public interface FacultyService {
     FacultyDto create(BaseFacultyDto facultyDto);
     List<FacultyDto> findAll();
     FacultyDto find(Long id);
+    void update(Long id, BaseFacultyDto facultyDto);
+    void patch(Long id, FacultyPatch facultyPatch);
+    void delete(Long id);
 }
