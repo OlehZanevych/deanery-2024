2aff73f2cd91c5badf2c24c063bac3836c86943d
Oleh Zanevych <ozanevych@ancestry.com>
2024-03-06 09:49:45 +0200

    Added support of count enpoint and filtering options for Faculty

--- src/main/java/org/lnu/teaching/web/application/dising/deanery/service/faculty/FacultyService.java
+++ src/main/java/org/lnu/teaching/web/application/dising/deanery/service/faculty/FacultyService.java
@@ -1,14 +1,17 @@
 package org.lnu.teaching.web.application.dising.deanery.service.faculty;
 
+import org.lnu.teaching.web.application.dising.deanery.dto.common.ValueDto;
 import org.lnu.teaching.web.application.dising.deanery.dto.faculty.BaseFacultyDto;
 import org.lnu.teaching.web.application.dising.deanery.dto.faculty.FacultyDto;
 import org.lnu.teaching.web.application.dising.deanery.dto.faculty.FacultyPatch;
+import org.lnu.teaching.web.application.dising.deanery.dto.faculty.query.params.FacultyFilterOptions;
 
 import java.util.List;
 
 public interface FacultyService {
     FacultyDto create(BaseFacultyDto facultyDto);
-    List<FacultyDto> findAll();
+    List<FacultyDto> findAll(FacultyFilterOptions filterOptions, Integer limit, Integer offset);
+    ValueDto<Integer> count(FacultyFilterOptions filterOptions);
     FacultyDto find(Long id);
     void update(Long id, BaseFacultyDto facultyDto);
     void patch(Long id, FacultyPatch facultyPatch);
