31c9a8d3dcec66e0a2738e569cde14766e83df44
Oleh Zanevych <ozanevych@ancestry.com>
2024-03-20 10:04:02 +0200

    Added more operations to User Entity

--- src/main/java/org/lnu/teaching/web/application/dising/deanery/service/faculty/user/UserService.java
+++
@@ -1,8 +0,0 @@
-package org.lnu.teaching.web.application.dising.deanery.service.faculty.user;
-
-import org.lnu.teaching.web.application.dising.deanery.dto.user.UserCreateDto;
-import org.lnu.teaching.web.application.dising.deanery.dto.user.UserDto;
-
-public interface UserService {
-    UserDto create (UserCreateDto userDto);
-}
